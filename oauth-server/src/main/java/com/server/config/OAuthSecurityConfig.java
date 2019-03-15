e041ee2b5909f0f8fdf23d67be308d938fe6da88
fanjc <440599256@qq.com>
2019-03-15 17:26:34 +0800

    add oauth-server

---
+++ oauth-server/src/main/java/com/server/config/OAuthSecurityConfig.java
@@ -0,0 +1,138 @@
+package com.service.auth.serviceauth.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+
+/** 
+* @Description: 
+* @param:
+* @return: 
+* @author: fanjc
+* @Date: 2019/3/11 
+*/ 
+@Configuration
+@EnableResourceServer
+@EnableAuthorizationServer
+public class OAuthSecurityConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private TokenEndpoint tokenEndpoint;
+
+
+    static final Logger logger = LoggerFactory.getLogger(OAuthSecurityConfig.class);
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    /**
+    * @Description: JWT 密钥非对称加密
+    * @param:
+    * @return:
+    * @author: fanjc
+    * @Date: 2019/3/12
+    */
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("test-jwt.jks"), "test123".toCharArray());
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("test-jwt"));
+        return converter;
+    }
+
+    @Bean // 声明 ClientDetails实现
+    public ClientDetailsService clientDetailsService() {
+        return new JdbcClientDetailsService(dataSource);
+    }
+
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        String finalSecret = "++++++" + new BCryptPasswordEncoder().encode("123456");
+        System.out.println(finalSecret);
+        clients.withClientDetails(clientDetailsService);
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore).tokenEnhancer(jwtAccessTokenConverter()).authenticationManager(authenticationManager);
+
+        // 配置tokenServices参数
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(endpoints.getTokenStore());
+        tokenServices.setSupportRefreshToken(false);
+        tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
+        tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
+        tokenServices.setAccessTokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(30)); // 30天
+        endpoints.tokenServices(tokenServices);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        // 允许表单认证
+        security.allowFormAuthenticationForClients()
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()")
+                .passwordEncoder(new BCryptPasswordEncoder());  // client_secret用BCrypt加密
+    }
+
+   /**
+   * @Description: 获取token时接受get方式
+   * @param:
+   * @return:
+   * @author: fanjc
+   * @Date: 2019/3/12
+   */
+    @PostConstruct
+    public void reconfigure() {
+        Set<HttpMethod> allowedMethods =
+                new HashSet<>(Arrays.asList(HttpMethod.GET, HttpMethod.POST));
+        tokenEndpoint.setAllowedRequestMethods(allowedMethods);
+    }
+}
---
+++ oauth-server/src/main/java/com/server/config/OAuthWebConfig.java
@@ -0,0 +1,65 @@
+package com.service.auth.serviceauth.config;
+
+import com.service.auth.serviceauth.dto.UserServiceDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+/** 
+* @Description: 
+* @param:
+* @return: 
+* @author: fanjc
+* @Date: 2019/3/11 
+*/ 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class OAuthWebConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+//        return  new Md5PasswordEncoder();
+    }
+
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.requestMatchers().anyRequest()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/**","/oauth/authorize").permitAll();
+        http.formLogin();
+    }
+
+    @Autowired
+    UserServiceDetail userServiceDetail;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userServiceDetail).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+}
